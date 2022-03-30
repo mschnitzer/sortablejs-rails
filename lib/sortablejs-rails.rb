cbe052e98a98778a99a46c616bc11e8866b8ad77
Manuel Schnitzer <manuel.schnitzer@verosent.com>
2022-03-30 17:07:59 +0200

    Fix ActiveSupport::XmlMini::IsolatedExecutionState error

--- lib/sortablejs-rails.rb
+++ lib/sortablejs-rails.rb
@@ -1,6 +1,7 @@
+require 'active_support'
 require_relative 'sortablejs-rails/rails.rb'
 
 module SortableJS
-  VERSION            = '1.14.0' # version of this gem
-  SORTABLEJS_VERSION = '1.14.0' # version of the library (https://github.com/SortableJS/Sortable)
+  VERSION            = '1.15.0' # version of this gem
+  SORTABLEJS_VERSION = '1.15.0' # version of the library (https://github.com/SortableJS/Sortable)
 end
